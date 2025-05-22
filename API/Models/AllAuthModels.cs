f088249a15cb207e4e793dcc24fde4a840afe565
Niyazi <nyztekin@hotmail.com>
2025-05-22 10:52:01 +0300

    JWT ayarları eklendi ve User sınıfı oluşturuldu.
    `appsettings.json` dosyasına JWT ayarları eklendi. Bu ayarlar, uygulamanın JWT ile kimlik doğrulama ve yetkilendirme işlemlerini gerçekleştirmesine olanak tanır.
    
    `AllAuthModels.cs` dosyasına yeni bir `User` sınıfı eklendi. Bu sınıf, kullanıcı bilgilerini tutmak için gerekli olan `Id`, `Username`, `PasswordHash`, `PasswordSalt`, `Role` ve `IsActive` gibi özellikleri içerir. Ayrıca, `Username`, `PasswordHash` ve `PasswordSalt` alanları için zorunlu kısıtlamalar ve `Username` için maksimum uzunluk kısıtlaması eklenmiştir.

---
+++ API/Models/AllAuthModels.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Username { get; set; }
+
+        [Required]
+        public byte[] PasswordHash { get; set; }
+
+        [Required]
+        public byte[] PasswordSalt { get; set; }
+
+        [MaxLength(50)]
+        public string Role { get; set; } // "Admin", "Operator", "CustomerRep", vs.
+
+        //public int? CompanyId { get; set; }
+        //public Company Company { get; set; }
+
+        public bool IsActive { get; set; } = true;
+    }
+
+}
